b994b49f7183e269591ef8d1a9a541850f81fe9a
Adam Dangoor <adamdangoor@gmail.com>
2018-08-12 02:36:00 +0100

    More stubs

--- acdtools.py
+++ acdtools.py
@@ -3,11 +3,19 @@ from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
+# TODO make public functions click commands
 
-def acdtools_unmount(mountpoint: Path):
+
+# TODO make this run on the group
+def _dependency_check():
+    # TODO fill in
     pass
 
-def acdtools_unmount():
+def _unmount(mountpoint: Path):
+    # TODO fill in
+    pass
+
+def unmount_all():
     message = 'Unmounting all ACDTools moundpoints'
     logger.info(message)
 
@@ -19,4 +27,22 @@ def acdtools_unmount():
     )
 
     for mountpoint in mountpoints:
+        _unmount(mountpoint)
+
+
+def sync_nodes():
+    # TODO fill in
+    pass
+
+def sync_deletes():
+    # TODO fill in
+    pass
+
+def mount():
+    unmount_all()
+    sync_nodes()
+    # TODO the actual mount
+
+def acd_cli_mount():
+    # TODO fill in
     pass
